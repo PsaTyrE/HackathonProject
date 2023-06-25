a87b4b7b03e48c919b76c89ac36d814f346b5da6
PsaTyrE <113742594+PsaTyrE@users.noreply.github.com>
2023-06-25 14:06:50 +0700

    Update script.sh

--- bash_script/script.sh
+++ bash_script/script.sh
@@ -1,3 +1,2 @@
 # Define the list of IDS rules
-alert icmp any any <> 192.168.0.232 any (msg:"Ada yang ECHO PING"; icode:0; itype:8; sid: 1000002; )
-alert icmp any any <> 192.168.0.232 any (msg:"Ada yang ECHO REPLY PING"; icode:0; itype:0; sid: 1000003; )
+alert tcp any any -> 192.168.10.0 23 (msg: "Ada yang telnet ke mesin!"; sid:1000001;)
