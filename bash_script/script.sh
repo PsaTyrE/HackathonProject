f9408734491c09f31bf20733164a883445a32c5c
PsaTyrE <113742594+PsaTyrE@users.noreply.github.com>
2023-06-25 13:55:46 +0700

    Update script.sh

--- bash_script/script.sh
+++ bash_script/script.sh
@@ -1,8 +1,3 @@
 # Define the list of IDS rules
 alert tcp any any -> 192.168.10.0 any (msg:\"Example IDS rule 1\"; content:\"example content\";)
 alert udp any any -> 192.168.10.0 any (msg:\"Example IDS rule 2\"; content:\"example content\";)
-
-# Define the list of firewall rules
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
