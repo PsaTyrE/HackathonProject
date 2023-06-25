9472419fcc79c4bf5f19d78edb8454c802f03e92
PsaTyrE <113742594+PsaTyrE@users.noreply.github.com>
2023-06-25 14:01:20 +0700

    Update script.sh

--- bash_script/script.sh
+++ bash_script/script.sh
@@ -1,3 +1,3 @@
 # Define the list of IDS rules
-alert tcp any any -> 192.168.10.0 any (msg:\"Example IDS rule 1\"; content:\"example content\";)
-alert udp any any -> 192.168.10.0 any (msg:\"Example IDS rule 2\"; content:\"example content\";)
+alert icmp any any <> 192.168.0.232 any (msg:"Ada yang ECHO PING"; icode:0; itype:8; sid: 1000002; )
+alert icmp any any <> 192.168.0.232 any (msg:"Ada yang ECHO REPLY PING"; icode:0; itype:0; sid: 1000003; )
